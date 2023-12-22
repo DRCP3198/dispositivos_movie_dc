8cd5b37d393264c229046c43debe86e52fd72c60
Dillan Coloma <renatocoloma2010@gmail.com>
2023-12-22 16:54:07 -0500

    Inicio trabajo Dispositivos Movie

---
+++ app/src/main/java/com/biometric/movies_app_dc/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.biometric.movies_app_dc.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.biometric.movies_app_dc.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
